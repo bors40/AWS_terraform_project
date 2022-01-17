f7a22a6f4d34803a0589457f6346f43527f7b689
Benji <87788154+bhht91@users.noreply.github.com>
2022-01-16 21:08:16 -0500

    ec2 trigger trial 1

--- default_page.tf
+++ default_page.tf
@@ -33,3 +33,13 @@ EOF
     Name = "prdnx-default"
   }
 }
+
+resource "aws_instance" "my-first-ec2" {
+  ami           = "ami-08e4e35cccc6189f4"
+  instance_type = "t2.micro"
+  key_name      = "benji91"
+  tags = {
+    Name  = "myec2-1"
+    Owner = "Burhan"
+  }
+}
